67aa8cd13cad9fb221f4db855d9802492780dd0f
Julia Nikitina <julianikitina.ios@gmail.com>
2018-09-05 22:34:57 +0300

    Set up the project

---
+++ Pila/Pila/Controllers/LoginPromptScreen/LoginPromptViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginPromptViewController.swift
+//  Pila
+//
+//  Created by Julia Nikitina on 05/09/2018.
+//  Copyright © 2018 Julia Nikitina. All rights reserved.
+//
+
+import UIKit
+
+class LoginPromptViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Pila/Pila/Controllers/LoginPromptScreen/LoginPromptViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  LoginPromptViewModel.swift
+//  Pila
+//
+//  Created by Julia Nikitina on 05/09/2018.
+//  Copyright © 2018 Julia Nikitina. All rights reserved.
+//
+
+import Foundation
