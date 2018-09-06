0204237c292640af27e334e6b7f350247582dd5c
Julia Nikitina <julianikitina.ios@gmail.com>
2018-09-06 21:30:16 +0300

    Add login and registration screen files

---
+++ Pila/Pila/Controllers/LoginAndRegistrationScreen/LoginAndRegistrationViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginAndRegistrationViewController.swift
+//  Pila
+//
+//  Created by Julia Nikitina on 06/09/2018.
+//  Copyright © 2018 Julia Nikitina. All rights reserved.
+//
+
+import UIKit
+
+class LoginAndRegistrationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Pila/Pila/Controllers/LoginAndRegistrationScreen/LoginAndRegistrationViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  LoginAndRegistrationViewModel.swift
+//  Pila
+//
+//  Created by Julia Nikitina on 06/09/2018.
+//  Copyright © 2018 Julia Nikitina. All rights reserved.
+//
+
+import Foundation
