b7c0537f65b436ea7b635a73567d42cf1436d917
Julia Nikitina <julianikitina.ios@gmail.com>
2018-09-08 14:38:53 +0300

    Add fonts

---
+++ Pila/Pila/Constants/Fonts.swift
@@ -0,0 +1,25 @@
+//
+//  Constants.swift
+//  Pila
+//
+//  Created by Julia Nikitina on 08/09/2018.
+//  Copyright © 2018 Julia Nikitina. All rights reserved.
+//
+
+import UIKit
+
+struct Fonts {
+    
+    static func latoRegular(size: CGFloat) -> UIFont {
+        return UIFont(name: "Lato-Regular", size: size)
+    }
+    
+    static func latoBold(size: CGFloat) -> UIFont {
+        return UIFont(name: "Lato-Bold", size: size)
+    }
+    
+    static func gilroy(size: CGFloat) -> UIFont {
+        return UIFont(name: "Gilroy-ExtraBold", size: size)
+    }
+    
+}
